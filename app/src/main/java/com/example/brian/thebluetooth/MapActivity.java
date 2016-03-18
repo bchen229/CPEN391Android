f63e94987126722d860605ecf65669776043657f
david <davidtpownall@alumni.ubc.ca>
2016-03-17 18:53:04 -0700

    Updated fullscreen maps pt 2

---
+++ app/src/main/java/com/example/brian/thebluetooth/MapActivity.java
@@ -0,0 +1,36 @@
+package com.example.brian.thebluetooth;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng van = new LatLng(49, -123);
+        mMap.addMarker(new MarkerOptions().position(van).title("West Coast"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(van));
+    }
+}
