40c21fdb632ff307e0b03b2325d4a9ad785ee39c
brian618 <brianc-@live.ca>
2016-03-17 17:13:56 -0700

    added bluetooth function

---
+++ app/src/main/java/com/example/brian/thebluetooth/BluetoothAttempt.java
@@ -0,0 +1,307 @@
+package com.example.brian.thebluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.UUID;
+
+public class BluetoothAttempt extends AppCompatActivity {
+
+    // two dynamic arrays of strings (populate at run time)
+    private ArrayList<String> myPairedDevicesStringArray = new ArrayList<String>();
+    private ArrayList<String> myDiscoveredDevicesStringArray = new ArrayList<String>();
+
+    private BroadcastReceiver mReceiver ; // handle to BroadCastReceiver object
+
+    private ArrayList < BluetoothDevice > Paireddevices = new ArrayList < BluetoothDevice > ( ) ;
+    private ArrayList <BluetoothDevice> Discovereddevices = new ArrayList < BluetoothDevice > ( ) ;
+
+    // A constant that we use to determine if our request to turn on bluetooth worked
+    private final static int REQUEST_ENABLE_BT = 1;
+    // A handle to the tablet’s bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter;
+    // get the context for the application. We use this with things like "toast" popups
+    private Context context;
+
+    // two instances of our new custom array adaptor
+    private MyCustomArrayAdaptor myPairedArrayAdapter;
+    private MyCustomArrayAdaptor myDiscoveredArrayAdapter;
+
+    private AdapterView.OnItemClickListener mPairedClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            // position = row number that user touched
+            // setValid(…) is a user written function in the custom array adaptor class
+            myPairedArrayAdapter.setValid(position);
+            // inform array adaptor that data has changed
+            myPairedArrayAdapter.notifyDataSetChanged();
+        }
+    };
+
+    private AdapterView.OnItemClickListener mDiscoveredClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            // get the details of the device name etc.
+            String text = "Discovered Device: " +
+                    myDiscoveredDevicesStringArray.get ( position );
+            Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+
+            // we are going to connect to the other device as a client
+            // if we are already connected to a device, close connections
+            if(Connected == true)
+                closeConnection(); // user defined fn to close streams and socket
+
+            // get the selected bluetooth device based on position then connect to it
+            // see page 24 and 25
+            CreateSerialBluetoothDeviceSocket( Discovereddevices.get (position) ) ;
+            ConnectToSerialBlueToothDevice(); // user defined fn
+
+            // update the view of discovered devices if required
+            myDiscoveredArrayAdapter.notifyDataSetChanged();
+
+        }
+    };
+
+    // a “socket” to a blue tooth device
+    private BluetoothSocket mmSocket = null;
+    // input/output “streams” with which we can read and write to device
+    // use of “static” important, it means variables can be accessed
+    // without an object, this is useful as other activities can use
+    // these streams to communicate after they have been opened.
+    public static InputStream mmInStream = null;
+    public static OutputStream mmOutStream = null;
+    // indicates if we are connected to a device
+    private boolean Connected = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_blue_tooth_attempt);
+
+        // get the context for the application
+        context = getApplicationContext();
+        // This call returns a handle to the one bluetooth device within your Android device
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // check to see if your android device even has a bluetooth device !!!!,
+        if (mBluetoothAdapter == null) {
+            Toast toast = Toast.makeText(context, "No Bluetooth !!", Toast.LENGTH_LONG);
+            toast.show();
+            finish(); // if no bluetooth device on this tablet don’t go any further.
+            return ;
+        }
+
+        // create the new adaptors passing important params, such
+        // as context, android row style and the array of strings to display
+        myPairedArrayAdapter = new MyCustomArrayAdaptor(this,
+                android.R.layout.simple_list_item_1, myPairedDevicesStringArray);
+        myDiscoveredArrayAdapter = new MyCustomArrayAdaptor(this,
+                android.R.layout.simple_list_item_1, myDiscoveredDevicesStringArray);
+
+        // get handles to the two list views in the Activity main layout
+        ListView PairedlistView = (ListView) findViewById( R.id.pairedDevicesList );
+        ListView DiscoveredlistView = (ListView) findViewById( R.id.discoveredDevicesList );
+
+        // add some action listeners for when user clicks on row in either list view
+        PairedlistView.setOnItemClickListener (mPairedClickedHandler);
+        DiscoveredlistView.setOnItemClickListener (mDiscoveredClickedHandler);
+
+        // set the adaptor view for both list views above
+        PairedlistView.setAdapter (myPairedArrayAdapter);
+        DiscoveredlistView.setAdapter (myDiscoveredArrayAdapter);
+
+        // notify the array adaptor that the array contents have changed (redraw)
+        myDiscoveredArrayAdapter.notifyDataSetChanged();
+
+        // If the bluetooth device is not enabled, let’s turn it on
+        if (!mBluetoothAdapter.isEnabled()) {
+        // create a new intent that will ask the bluetooth adaptor to “enable” itself.
+        // A dialog box will appear asking if you want turn on the bluetooth device
+            Intent enableBtIntent = new Intent( BluetoothAdapter.ACTION_REQUEST_ENABLE );
+
+        // REQUEST_ENABLE_BT below is a constant (defined as '1 - but could be anything)
+        // When the “activity” is run and finishes, Android will run your onActivityResult()
+        // function (see next page) where you can determine if it was successful or not
+
+            startActivityForResult (enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive (Context context, Intent intent) {
+                String action = intent.getAction();
+                BluetoothDevice newDevice;
+
+                if ( action.equals(BluetoothDevice.ACTION_FOUND) ) { // If a new BT device found
+                    // Intent will contain discovered Bluetooth Device so go and get it
+                    newDevice = intent.getParcelableExtra ( BluetoothDevice.EXTRA_DEVICE );
+
+                    // Add the name and address to the custom array adapter to show in a ListView
+                    String theDevice = new String( newDevice.getName() +
+                            "\nMAC Address = " + newDevice.getAddress());
+
+                    Toast.makeText(context, theDevice, Toast.LENGTH_LONG).show();
+
+                    //add the new device and string details to the two arrays (page 15)
+                    Discovereddevices.add ( newDevice );
+                    myDiscoveredDevicesStringArray.add ( theDevice );
+
+                    // notify array adaptor that the contents of String Array have changed
+                    myDiscoveredArrayAdapter.notifyDataSetChanged ();
+                }
+                // visual feedback for user
+                else if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_STARTED)) {
+                    Toast.makeText(context, "Discovery Started", Toast.LENGTH_LONG).show();
+                }
+                else if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED) ) {
+                    Toast.makeText(context, "Discovery Finished", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        // create 3 separate IntentFilters that are tuned to listen to certain Android broadcasts
+        // 1) when new Bluetooth devices are discovered,
+        // 2) when discovery of devices starts (not essential but give useful feedback)
+        // 3) When discovery ends (not essential but give useful feedback)
+        IntentFilter filterFound = new IntentFilter (BluetoothDevice.ACTION_FOUND);
+        IntentFilter filterStart = new IntentFilter (BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        IntentFilter filterStop = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        // register our broadcast receiver so it gets called every time
+        // a new bluetooth device is found or discovery starts or finishes
+        // we should unregister it again when the app ends in onDestroy() - see later
+        registerReceiver (mReceiver, filterFound);
+        registerReceiver (mReceiver, filterStart);
+        registerReceiver (mReceiver, filterStop);
+
+        Set< BluetoothDevice > thePairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are devices that have already been paired
+        // get an iterator for the set of devices and iterate 1 device at a time
+        if (thePairedDevices.size() > 0) {
+            Iterator<BluetoothDevice> iter = thePairedDevices.iterator() ;
+            BluetoothDevice aNewdevice ;
+            while ( iter.hasNext() ) { // while at least one more device
+                aNewdevice = iter.next(); // get next element in set
+                // Add the name and address to an array adapter to show in a ListView
+                String PairedDevice = new String( aNewdevice.getName ()
+                        + "\nMAC Address = " + aNewdevice.getAddress ());
+
+                //add the new device details to the array
+                Paireddevices.add (aNewdevice);
+                myPairedDevicesStringArray.add (PairedDevice);
+                myPairedArrayAdapter.notifyDataSetChanged();
+            }
+        }
+
+        // Before starting discovery make sure discovery is cancelled
+        if (mBluetoothAdapter.isDiscovering())
+            mBluetoothAdapter.cancelDiscovery();
+        // now start scanning for new devices. The broadcast receiver
+        // we wrote earlier will be called each time we discover a new device
+        // don't make this call if you only want to show paired devices
+        mBluetoothAdapter.startDiscovery();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver ( mReceiver ); // make sure we unregister
+        // our broadcast receiver
+    }
+
+    void closeConnection() {
+        try {
+            mmInStream.close();
+            mmInStream = null;
+        } catch (IOException e) {}
+        try {
+            mmOutStream.close();
+            mmOutStream = null;
+        } catch (IOException e) {}
+        try {
+            mmSocket.close();
+            mmSocket = null;
+        } catch (IOException e) {}
+
+        Connected = false ;
+    }
+
+    public void CreateSerialBluetoothDeviceSocket(BluetoothDevice device)
+    {
+        mmSocket = null;
+
+        // universal UUID for a serial profile RFCOMM blue tooth device
+        // this is just one of those “things” that you have to do and just works
+        UUID MY_UUID = UUID.fromString ("00001101-0000-1000-8000-00805F9B34FB");
+        // Get a Bluetooth Socket to connect with the given BluetoothDevice
+        try {
+            // MY_UUID is the app's UUID string, also used by the server code
+            mmSocket = device.createRfcommSocketToServiceRecord (MY_UUID);
+        }
+        catch (IOException e) {
+            Toast.makeText(context, "Socket Creation Failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void ConnectToSerialBlueToothDevice() {
+        // Cancel discovery because it will slow down the connection
+        mBluetoothAdapter.cancelDiscovery();
+        try {
+            // Attempt connection to the device through the socket.
+            mmSocket.connect();
+            Toast.makeText(context, "Connection Made", Toast.LENGTH_LONG).show();
+        }
+        catch (IOException connectException) {
+            Toast.makeText(context, "Connection Failed", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //create the input/output stream and record fact we have made a connection
+        GetInputOutputStreamsForSocket();
+        Connected = true ;
+    }
+
+    // gets the input/output stream associated with the current socket
+    public void GetInputOutputStreamsForSocket() {
+        try {
+            mmInStream = mmSocket.getInputStream();
+            mmOutStream = mmSocket.getOutputStream();
+        } catch (IOException e) { }
+    }
+}
---
+++ app/src/main/java/com/example/brian/thebluetooth/MyCustomArrayAdaptor.java
@@ -0,0 +1,68 @@
+package com.example.brian.thebluetooth;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Brian on 2016-03-16.
+ */
+public class MyCustomArrayAdaptor extends ArrayAdapter<String> {
+    // my new class variables, copies of constructor params, but add more if required
+    private Context context;
+    private ArrayList<String> theStringArray;
+    public final int numRows = 500 ;
+    private boolean [] RowValidity = new boolean [numRows];
+
+    // constructor
+    public MyCustomArrayAdaptor(Context _context,
+                                int textViewResourceId,
+                                ArrayList<String> _theStringArray)
+    {
+        // call base class constructor
+        super(_context, textViewResourceId, _theStringArray);
+        // save the context and the array of strings we were given
+        context = _context;
+        theStringArray = _theStringArray;
+        clearValidity ();
+    }
+
+    @Override
+    public View getView (int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE );
+        View row = inflater.inflate ( R.layout.row, parent, false );
+        ImageView icon = (ImageView) row.findViewById (R.id.BTicon);
+        icon.setImageResource (R.drawable.bluetooth);
+        icon.setVisibility (View.VISIBLE);
+        TextView label = (TextView) row.findViewById( R.id.BTdeviceText);
+        label.setText (theStringArray.get(position));
+        icon = (ImageView) row.findViewById (R.id.Selected);
+        icon.setImageResource (R.drawable.redcross);
+        icon.setVisibility (View.VISIBLE);
+
+        if(RowValidity [position] == false)
+            icon.setImageResource (R.drawable.redcross);
+        else
+            icon.setImageResource (R.drawable.greentick);
+
+        return row;
+    }
+
+    public void setValid (int position) { RowValidity [position] = true ; }
+
+    public void setInValid (int position) { RowValidity [position] = false ; }
+
+    public void clearValidity () {
+        for(int i = 0; i < numRows; i ++)
+            RowValidity[i] = false ;
+    }
+}
