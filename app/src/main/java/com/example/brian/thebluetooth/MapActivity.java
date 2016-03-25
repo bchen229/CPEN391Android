80e0c3b9dedac722e22a0f2fbe16116483d82abc
david <davidtpownall@alumni.ubc.ca>
2016-03-24 19:42:09 -0700

    Asynchronous GPS Bluetooth Polling for map

--- app/src/main/java/com/example/brian/thebluetooth/MapActivity.java
+++ app/src/main/java/com/example/brian/thebluetooth/MapActivity.java
@@ -1,7 +1,11 @@
 package com.example.brian.thebluetooth;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +16,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+
 public class MapActivity extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private MapView mapView;
+    private Thread thread;
+    private Handler handler = new Handler();
+    private String TAG = "Thread Task";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         View view = inflater.inflate(R.layout.activity_map, container, false);
 
         // create the map
@@ -28,6 +39,14 @@ public class MapActivity extends Fragment implements OnMapReadyCallback {
 
         mapView.getMapAsync(this);
 
+        thread = new Thread() {
+            public void run() {
+                GPSHandler();
+                Log.d(TAG, "GPS Updated");
+                handler.postDelayed(this, 1000);
+            }
+        };
+        thread.run();
         return view;
     }
 
@@ -58,4 +77,67 @@ public class MapActivity extends Fragment implements OnMapReadyCallback {
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
+    //
+// This function write a line of text (in the form of an array of bytes)
+// to the Bluetooth device and then sends the string “\r\n”
+// (required by the bluetooth dongle)
+//
+    public void WriteToBTDevice (String message) {
+        String s = "\r\n" ;
+        byte[] msgBuffer = message.getBytes();
+        byte[] newline = s.getBytes();
+
+        try {
+            BluetoothAttempt.mmOutStream.write(msgBuffer) ;
+            BluetoothAttempt.mmOutStream.write(newline) ;
+        } catch (IOException e) { }
+    }
+
+    public String ReadFromBTDevice() {
+        byte c;
+        String s = "";
+
+        try { // Read from the InputStream using polling and timeout
+            for (int i = 0; i < 200; i++) { // try to read for 2 seconds max
+                SystemClock.sleep(10);
+                if (BluetoothAttempt.mmInStream.available() > 0) {
+                    if ((c = (byte) BluetoothAttempt.mmInStream.read()) != '\r') // '\r' terminator
+                        s += (char) c; // build up string 1 byte by byte
+                    else
+                        return s;
+                }
+            }
+        } catch (IOException e) {
+            return "-- No Response --";
+        }
+        return s;
+    }
+
+    private void GPSHandler(){
+
+        String check;
+        String command = ";GPS;";
+        String coordinates;
+        String send = ";send;";
+
+        //Fix this once we have bluetooth
+
+       /* WriteToBTDevice(command);
+
+        if((check = ReadFromBTDevice()).equals("ack")){
+
+            WriteToBTDevice(send);
+
+            coordinates = ReadFromBTDevice();
+
+            //Parse Here
+            //Post Here
+
+
+        }
+*/
+
+    }
+
 }
